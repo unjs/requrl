fe9e3c5cf4268506d53270414a39dd1d3fc9baba
Pooya Parsa <pyapar@gmail.com>
2019-11-25 13:16:46 +0100

    refactor: typescript

--- urlful.ts
+++ urlful.ts
@@ -1,8 +1,11 @@
+import { IncomingMessage } from 'http'
 import isHTTPS from 'is-https'
 
-const _location = typeof location !== 'undefined' ? location : {}
+const _location = typeof location !== 'undefined' ? location : {
+  href: undefined
+}
 
-export default function getURL (req) {
+export default function getURL (req: IncomingMessage) : string {
   if (!req) {
     return _location.href
   }
